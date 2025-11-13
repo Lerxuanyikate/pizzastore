29581d0f273a05f530911f9b18c19acc0838c1bd
Lerxuanyikate <kateler6517@gmail.com>
2025-11-13 10:15:42 +0800

    pizza

--- src/index.jsx
+++ src/index.jsx
@@ -6,26 +6,61 @@ function Header() {
     return <h1 style={{color: "red", fontSize: "42px", textAlign: "screenLeft"}}> Kate Ler's Pizza </h1>;
 }   
 
-function Pizza() {
-    return <div>
-        <img src="https://fratellisnewyorkpizza.com/wp-content/uploads/2019/07/spinach-pizzacheech.png" alt="Spinach Pizza" height={100}/>
-        <h3>Spinach Pizza</h3>
-        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-        <p>Quantity: 10</p>
-        </div>
+function Pizza( {
+    name, ingredients, price, photoName
+}) {
+    return <div className='pizza'>
+        <img src={photoName} alt={name} height={100}/>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <p>{price}</p>
+    </div>
+    
 };
 
 function Menu() {
-    return (
-        <div>
-            <h2>Our Menu</h2>
-            <Pizza/>
-            <Pizza/>
-            <Pizza/>
-            <Pizza/>
-        </div>
-    )
-}
+    const pizzaData = [
+            {
+                name: "Spinach", 
+                ingredients: "Tomato, Mozzarella, Spinach", 
+                price: 10, 
+                photoName: "pizza/spinach.png"
+            },
+            {
+                name: "Pepperoni", 
+                ingredients: "Tomato, Mozzarella, Pepperoni", 
+                price: 12, 
+                photoName: ""
+            },
+            {
+                name: "Hawaiian", 
+                ingredients: "Tomato, Mozzarella, Ham, Pineapple", 
+                price: 12, 
+                photoName: "https://example.com/hawaiian.jpg"
+            },
+            {
+                name: "Veggie", 
+                ingredients: "Tomato, Mozzarella, Bell Peppers, Olives, Onions", 
+                price: 9, 
+                photoName: "https://example.com/veggie.jpg"
+            },
+        ];
+        return (
+            <div className="menu">
+                <h2>Our Menu</h2>
+                <div className="pizzas">
+                    {
+                    pizzaData.map(pizza => (
+                        <Pizza 
+                            key={pizza.name}
+                            {...pizza}
+                        />
+                    ))
+                    }
+                </div>
+            </div>
+        )
+    }
 
 function Footer() {
     const hour = new Date().getHours();
@@ -33,9 +68,16 @@ function Footer() {
     
     return (
         <footer className='footer'>
-            {isOpen ? "We're currently open!"  : "Sorry, we're closed now."}
+            {isOpen ? (
+                <div className="order">
+                <p>We're currently open!</p>
+                <button className='btn'>Order Now</button>
+                </div>
+            ) : (
+                <p>Sorry, we're closed now.</p>
+            )}
         </footer>
-    )
+    );
 }
 
 function App() {
